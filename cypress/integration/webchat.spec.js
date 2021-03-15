c5ba1f34ec973df56b9a35b350e6b8c6719a05ae
Kanakdeepa Pradhan <kanakdeepa@Kanakdeepas-MacBook-Air.local>
2021-03-15 16:56:41 +0800

    Tested personal info input

--- cypress/integration/webchat.spec.js
+++ cypress/integration/webchat.spec.js
@@ -1,6 +1,21 @@
 describe('Webchat interaction', () => {
-  it('Open webchat url', () => {
+  it('Open webchat window', () => {
     cy.visit('https://kkh-uat.app.keyreply.com/webchat/');
+  });
+
+  it('Get webchat window ready', () => {
     cy.get('#kr-launcher').click();
+    cy.contains('I Agree').click();
+  });
+
+  it('Accept personal information', () => {
+    cy.fixture('personal-info.json').then((data) => {
+      if (data && data.length > 0) {
+        data.forEach((record) => {
+          cy.get('#keyreply-panel-body').contains(record.question);
+          cy.get('textarea.el-textarea__inner').type(record.input);
+        });
+      }
+    });
   });
 });
