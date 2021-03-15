3ebe124ff2dd7f460e892f84ba4996639acd94a7
Kanakdeepa Pradhan <kanakdeepa@Kanakdeepas-MacBook-Air.local>
2021-03-15 19:09:38 +0800

    Added new emergency tests

--- cypress/integration/webchat.spec.js
+++ cypress/integration/webchat.spec.js
@@ -1,4 +1,4 @@
-describe('Webchat interaction', () => {
+describe('Webchat - Privacy Policy', () => {
   it('Open webchat window', () => {
     cy.visit('https://kkh-uat.app.keyreply.com/webchat/');
   });
@@ -18,4 +18,43 @@ describe('Webchat interaction', () => {
       }
     });
   });
+
+  it(
+    'Show accepted privacy policy',
+    {
+      defaultCommandTimeout: 10000,
+    },
+    () => {
+      cy.contains(
+        'Thank you for accepting the Privacy Policy. Please be patient with me as this is a Beta enhancement. I will be able to handle more conditions and questions in the future.'
+      );
+      cy.contains(
+        'If your child has overseas travel history (in/out of Singapore) in the last 14 days or if your child has a contact with a confirmed patient who has COVID-19'
+      );
+      cy.contains(
+        'If you have been issued a stay home notice and have developed symptoms of fever, cough, running nose,'
+      );
+      cy.contains(
+        'If your child has been issued a home quarantine order and has developed any sign of illness'
+      );
+      cy.contains(
+        'To start, please select one of the below conditions, or describe your conditions such as "My child has fever'
+      );
+    }
+  );
 });
+
+describe(
+  'Webchat - Emergecy AtoZ',
+  {
+    defaultCommandTimeout: 10000,
+  },
+  () => {
+    it('Emergency Test - Seizure', () => {
+      cy.get('textarea.el-textarea__inner').type('My child has seizure{enter}');
+      cy.get('#keyreply-panel-body').contains(
+        'I detected an emergency condition. Is your child suffering from seizure, drowsiness (altered subconsciousness, confused state, abnormal behaviour and NOT tiredness) or is unresponsive?'
+      );
+    });
+  }
+);
